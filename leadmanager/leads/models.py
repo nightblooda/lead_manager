dd5b84e0d002b2904200d396f7e092b99370f332
nightblooda <himanshux7x7x7@gmail.com>
2021-01-08 12:27:13 +0530

    commit

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Lead(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
