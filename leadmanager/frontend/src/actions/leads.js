d18c7abbe525c285c80961aea9ac808cae260053
nightblooda <himanshux7x7x7@gmail.com>
2021-02-09 21:49:29 +0530

    commit

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios"
-import { createMessage } from './messages'
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types"
+import { createMessage, returnErrors } from './messages'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types"
 
 export const getLeads = () => dispatch => {
     axios
@@ -11,7 +11,7 @@ export const getLeads = () => dispatch => {
             payload: res.data
         })
     })
-    .catch(err => console.log(err))
+    .catch(err =>dispatch(returnErrors(err.response.data, err.response.status)))
 }
 
 export const deleteLead = (id) => dispatch => {
@@ -38,13 +38,6 @@ export const addLead = (lead) => dispatch => {
         })
     })
     .catch(err => {
-        const errors = {
-            msg: err.response.data,
-            status: err.response.status
-        }
-        dispatch({
-            type: GET_ERRORS,
-            payload: errors
-        })
+        dispatch(returnErrors(err.response.data, err.response.status))
     })
 }
--- leadmanager/frontend/src/actions/messages.js
+++ leadmanager/frontend/src/actions/messages.js
@@ -1,8 +1,16 @@
-import { CREATE_MESSAGE } from './types'
+import { CREATE_MESSAGE, GET_ERRORS } from './types'
 
 export const createMessage = msg => {
     return {
         type: CREATE_MESSAGE,
         payload: msg
+    }
+}
+
+// RETURN ERRORS
+export const returnErrors = (msg, status) => {
+    return {
+        type: GET_ERRORS,
+        payload: {msg, status}
     }
 }
